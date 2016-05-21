d8978d9a2fb70841812c5edd5fa15f2a68fb8eb2
franklin97 <franklinsalazar@mybihs.org>
2016-05-21 19:04:02 +0000

    changes done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
     
     
-    
+    var currentcountry;
     var countries = {
 
     Japan: [{name: 'Avocado', image: 'avocados.png'},
@@ -16,13 +16,35 @@ $(document).ready(function(){
             ]
     
     };
+    
+    
+    currentcountry = countries.Japan;
+    
+    for(var x=0; x < currentcountry.length; x++)
+    {
+        
+        var ingredient = currentcountry[x];
+        
+        
+        console.log(ingredient.name);
+    }
    //$('#plate').css('height', window.innerHeight/2);
    /* $('#next').click(function(){
         $('.container-fluid').show();
         $('')
     })
     */
-    
+    var ingredientCount = 1;
+    var maxIngredient = 6;
+    $('.ingredients').click(function(){
+        $('#spot' + ingredientCount + ' img').attr('src', this.src);
+        
+        if(ingredientCount <= maxIngredient){
+            ingredientCount++;
+        }else {
+            ingredientCount = 1;
+        }
+    })
     
     
     
