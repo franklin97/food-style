b7e59f1e2bffc4ba58b84ca4ea1a50b1c8c2d306
franklin97 <franklinsalazar@mybihs.org>
2016-05-21 17:33:29 +0000

    new things added

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// JavaScript File
+ // JavaScript File
+$(document).ready(function(){
+    
+    
+    
+    var countries = {
+
+    Japan: [{name: 'Avocado', image: 'avocados.png'},
+            {name: 'Bacon', image: 'Bacon.png'},
+            {name: 'Cucumbers', image: 'cucumbers.png'},
+            {name: 'Eel', image: 'eel.png'},
+            {name: 'Mustard', image: 'mustard.png'},
+            {name: 'Rice', image: 'rice.jpg'},
+            {name: 'Seaweed', image: 'seaweed.png'},
+            {name: 'Shrimp', image: 'shrimp.png'}
+            ]
+    
+    };
+   //$('#plate').css('height', window.innerHeight/2);
+   /* $('#next').click(function(){
+        $('.container-fluid').show();
+        $('')
+    })
+    */
+    
+    
+    
+    
+})
